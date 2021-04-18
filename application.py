7503ac72c33c353663b408b38e2f5110cb8561ee
Megan Freedman <megan@freedman.co.nz>
2021-04-18 11:45:37 -0500

    completed with time added

--- application.py
+++ application.py
@@ -13,26 +13,31 @@ bootstrap = Bootstrap(application)
 
 def root():
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    table = dynamodb.Table('portaloo_data')
+    table = dynamodb.Table('portalooDataRecent')
     data = table.scan()
-    dictOfinput = {}
+    #dictOfinput = {}
     #data = table.query(KeyConditionExpression=Key('sample_time').eq('latest_entry_identifier'))
     tableItems = []
     items = []
     for i in data["Items"]:
         items.append(i)
     for key in items:
-        timeStamp = int(key["sample_time"])
-        time = datetime.datetime.fromtimestamp(timeStamp / 1e3)
+        #timeStamp = int(key["sample_time"])
+        #time = datetime.datetime.fromtimestamp(timeStamp / 1e3)
         device_data = key["device_data"]
         deviceId = int(key["device_id"])
-        if deviceId not in dictOfinput or dictOfinput[deviceId] < time:
-            dictOfinput[deviceId] = time
-            waterLevel = device_data["waterLevel"]
-            toiletPaperLevel = device_data["toiletPaperLevel"]
-            usage = device_data["usage"]
-            tableItems.append(Item(time, deviceId, waterLevel, toiletPaperLevel, usage))
+        #if deviceId not in dictOfinput or dictOfinput[deviceId] < time:
+        #    dictOfinput[deviceId] = time
+        waterLevel = device_data["waterLevel"]
+        toiletPaperLevel = device_data["toiletPaperLevel"]
+        usage = device_data["usage"]
+        humidity = device_data["humidity"]
+        temperature = device_data["temperature"]
+        orientation = device_data["orientation"]
+        sampleTime = device_data["sampleTime"]
+        tableItems.append(Item(deviceId, waterLevel, toiletPaperLevel, usage, humidity, temperature, orientation, sampleTime))
     table = ItemTable(tableItems, classes=["table"])
+
     return render_template("index.html", table=table)
 
 
@@ -40,29 +45,35 @@ def root():
 application.add_url_rule('/', 'index', (lambda : root()))
 # Declare your table
 class ItemTable(Table):
-    time = DatetimeCol('Time')
-    deviceID = Col('Device ID')
+    # todo fix why time is not working correctly
+    deviceID = Col('Portaloo ID')
     waterLevel = Col('Water level')
     toiletPaperLevel = Col("Toilet Paper Level")
-    usage = Col("Usage")
+    usage = Col("Usage since last service")
+    humidity = Col("Current humidity")
+    temperature = Col("Current temperature")
+    orientation = Col("Upright?")
+    sampleTime = Col("Sample Time")
 
     def get_tr_attrs(self, item):
         status = item.status()
         return {'class': status}
 
 
-
 # Get some objects
 class Item(object):
-    def __init__(self, time, deviceID, waterLevel, toiletPaperLevel, usage):
-        self.time = time
+    def __init__(self, deviceID, waterLevel, toiletPaperLevel, usage, humidity, temperature, orientation, sampleTime):
         self.deviceID = deviceID
         self.waterLevel = waterLevel
         self.toiletPaperLevel = toiletPaperLevel
         self.usage = usage
+        self.humidity = humidity
+        self.temperature = temperature
+        self.orientation = orientation
+        self.sampleTime = sampleTime
 
     def status(self):
-        if self.waterLevel <= 10 or self.toiletPaperLevel <= 10 or self.usage >= 100:
+        if self.waterLevel <= 10 or self.toiletPaperLevel <= 10 or self.usage >= 100 or self.orientation == False:
             return "table-danger"
         elif self.waterLevel >= 30 or self.toiletPaperLevel >= 30 or self.usage <= 20:
             return "table-success"
