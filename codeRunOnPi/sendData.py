5b5403ca0cfc1d64623cf01481639504a3baeeb3
Megan Freedman <megan@freedman.co.nz>
2021-04-20 11:31:16 -0500

    Updated code for py

--- codeRunOnPi/sendData.py
+++ codeRunOnPi/sendData.py
@@ -1,4 +1,4 @@
-##This runs on the pi, not
+
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
@@ -6,8 +6,10 @@ from awscrt import io, mqtt, auth, http
 from awsiot import mqtt_connection_builder
 from sense_hat import SenseHat
 import time as t
-from datetime import datetime
 import json
+import datetime
+import RPi.GPIO as GPIO
+import time
 
 sense = SenseHat()
 
@@ -17,10 +19,10 @@ CLIENT_ID = "testDevice"
 PATH_TO_CERT = "certificates/382dc9e2e4-certificate.pem.crt"
 PATH_TO_KEY = "certificates/382dc9e2e4-private.pem.key"
 PATH_TO_ROOT = "certificates/root.pem"
-DEVICE_ID = "20"
-TOPIC = "device/"+DEVICE_ID+"/data"
-POLL_TIME = 10
-
+DEVICE_ID = "60"
+TOPIC = "device/ " +DEVICE_ID +"/data"
+POLL_TIME = 600
+TANK_HEIGHT = 20
 
 def getTemperature():
     temp = sense.get_temperature()
@@ -42,24 +44,69 @@ def getOrientation():
         piUpright = False
     return (piUpright)
 
-def sendData(data):
 
+#Code from https://pimylifeup.com/raspberry-pi-distance-sensor/
+def getWaterLevel():
+    try:
+        GPIO.setmode(GPIO.BOARD)
+
+        PIN_TRIGGER = 7
+        PIN_ECHO = 11
+
+        GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+        GPIO.setup(PIN_ECHO, GPIO.IN)
+
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+        print("Waiting for sensor to settle")
+
+        t.sleep(2)
+
+        print("Calculating distance")
+
+        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+
+        t.sleep(0.00001)
+
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+        while GPIO.input(PIN_ECHO)==0 :
+            pulse_start_time = time.time()
+        while GPIO.input(PIN_ECHO)==1 :
+            pulse_end_time = time.time()
+
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = round(pulse_duration * 17150, 2)
+        print("Distance:",distance,"cm ")
+
+
+    finally:
+        GPIO.cleanup()
+        return ((TANK_HEIGHT - int(distance))/TANK_HEIGHT)*100
+
+
+
+
+#Loop to send data to AWS
+
+
+def sendData(data):
     # Spin up resources
     event_loop_group = io.EventLoopGroup(1)
     host_resolver = io.DefaultHostResolver(event_loop_group)
     client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
     mqtt_connection = mqtt_connection_builder.mtls_from_path(
-                endpoint=ENDPOINT,
-                cert_filepath=PATH_TO_CERT,
-                pri_key_filepath=PATH_TO_KEY,
-                client_bootstrap=client_bootstrap,
-                ca_filepath=PATH_TO_ROOT,
-                client_id=CLIENT_ID,
-                clean_session=False,
-                keep_alive_secs=6
-                )
+        endpoint=ENDPOINT,
+        cert_filepath=PATH_TO_CERT,
+        pri_key_filepath=PATH_TO_KEY,
+        client_bootstrap=client_bootstrap,
+        ca_filepath=PATH_TO_ROOT,
+        client_id=CLIENT_ID,
+        clean_session=False,
+        keep_alive_secs=6
+    )
     print("Connecting to {} with client ID '{}'...".format(
-            ENDPOINT, CLIENT_ID))
+        ENDPOINT, CLIENT_ID))
     # Make the connect() call
     connect_future = mqtt_connection.connect()
     # Future.result() waits until a result is available
@@ -73,14 +120,16 @@ def sendData(data):
     disconnect_future = mqtt_connection.disconnect()
     disconnect_future.result()
 
+
+#main loop gets data and sends, need to do water level, toilet paper level and usage sensors.
 while True:
     temperature = int(getTemperature())
     humidity = int(getHumidity())
     orientation = getOrientation()
-    waterLevel = 100
-    toiletPaperLevel = 20
-    usage = 20
-    time = datetime.now()
-    sendData({"sampleTime": time, waterLevel": waterLevel, "toiletPaperLevel": toiletPaperLevel, "usage": usage,
+    waterLevel = getWaterLevel()
+    toiletPaperLevel = 50
+    usage =50
+    sampleTime = datetime.datetime.now().isoformat()
+    sendData({"sampleTime": sampleTime, "waterLevel": waterLevel, "toiletPaperLevel": toiletPaperLevel, "usage": usage,
               "temperature": temperature, "humidity": humidity, "orientation": orientation})
     t.sleep(POLL_TIME)
